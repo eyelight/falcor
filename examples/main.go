3b4d552fbefa01d062e8f512cd9293e3bb6285d9
Jason Cochard <cochard@protonmail.com>
2023-11-30 12:44:37 -0800

    clarify example

--- examples/main.go
+++ examples/main.go
@@ -15,66 +15,74 @@ func init() {
 	}
 
 	// to use a luck dragon, you'll need to call WithLuck
-	atreyu := falcor.WithLuck(c)
+	artax := falcor.WithLuck(c)
 
 	// now system signals can ride falcor carrying a bag of lucky handlers
-	atreyu.Mount(syscall.SIGINT, "EXIT", func() {
+	artax.Mount(syscall.SIGINT, "EXIT", func() {
 		log.Println("Handling exit...")
 		os.Exit(0)
 	})
 
-	// subsequent calls to Mount will add functions to the same
-	// by defualt, they will be executed in order of calls to Mount, like a queue
-	atreyu.Mount(syscall.SIGINT, "SIGINT-init", func() {
+	// subsequent calls to Mount will add functions to the same dragon
+	// by defualt, functions will be executed concurrently
+	artax.Mount(syscall.SIGINT, "SIGINT-init", func() {
 		log.Println("Handling SIGINT-init")
 	})
 
 	// You can see your Riders (handled signals)
-	log.Printf("(init) Riders: %v", atreyu.Riders())
+	log.Printf("(init) Riders: %v", artax.Riders())
 }
 
 func main() {
 	// you can re-aquire your luck dragon with Luck
 	atreyu := falcor.Luck()
 
-	// and more calls to Mount with the same signal will affect the same Rider
+	// additional calls to Mount with the same signal will affect the same Rider as before
 	atreyu.Mount(syscall.SIGINT, "SIGINT-main", func() {
 		log.Println("Handling SIGINT-main")
 	})
 
-	// and new signals will ride the same luck dragon
+	// and new signals will ride the same luck dragon as a new Rider
 	atreyu.Mount(syscall.SIGHUP, "SIGHUP-main", func() {
 		log.Println("Handling SIGHUP")
 	})
 	log.Printf("(main) Riders: %v", atreyu.Riders())
-	log.Printf("[Falcor] Rider %s: Funcs (%s) Sequence (%s)", atreyu.Rider(syscall.SIGINT).String(), atreyu.Rider(syscall.SIGINT).Funcs(), atreyu.Rider(syscall.SIGINT).Sequence())
-	log.Printf("[Falcor] Rider %s: Funcs (%s) Sequence (%s)", atreyu.Rider(syscall.SIGHUP).String(), atreyu.Rider(syscall.SIGHUP).Funcs(), atreyu.Rider(syscall.SIGHUP).Sequence())
+	log.Printf("[Falcor] Rider %s: Funcs (%s) with sequence (%s)", atreyu.Rider(syscall.SIGINT).String(), atreyu.Rider(syscall.SIGINT).Funcs(), atreyu.Rider(syscall.SIGINT).Sequence())
+	log.Printf("[Falcor] Rider %s: Funcs (%s) with sequence (%s)", atreyu.Rider(syscall.SIGHUP).String(), atreyu.Rider(syscall.SIGHUP).Funcs(), atreyu.Rider(syscall.SIGHUP).Sequence())
 
 	// When you're ready to fly, you can
 	atreyu.Fly()
+	// and now falcor will be listening for the signals of its rider(s)
 
 	time.Sleep(1 * time.Second)
 
 	// You can Dismount at any time, reducing the number of functions in a Rider
 	atreyu.Dismount(syscall.SIGINT, "SIGINT-init")
 
-	// Or removing a Rider entirely if you dismount all your handlers
-	// atreyu.Dismount(syscall.SIGHUP, "SIGHUP-main")
+	// Or, remove a Rider entirely if you dismount all handlers from a Rider
+	atreyu.Dismount(syscall.SIGHUP, "SIGHUP-main")
 
-	// And if you want your multi-handler Riders to execute like a stack, queue, or concurrent (default), you can do that
+	// And if you want your multi-handler Riders to execute differently, you can
+	// choose stacklike (falcor.LIFO), queuelike (falcor.FIFO), or default (falcor.Concurrent)
 	atreyu.Rider(syscall.SIGINT).Execution(falcor.LIFO)
+
+	// try a ctl-x here
 
 	time.Sleep(10 * time.Second)
 	// If you want to stop all your system responders, you can land your luck dragon
 	atreyu.Land()
 
-	time.Sleep(3 * time.Second)
+	// try a ctl-x here, too
+
+	time.Sleep(10 * time.Second)
 
 	// You can fly again when ready
 	atreyu.Fly()
 
 	log.Printf("(main) Riders: %v", atreyu.Riders())
 	log.Printf("[Falcor] Rider %s: Funcs (%s) Sequence (%s)", atreyu.Rider(syscall.SIGINT).String(), atreyu.Rider(syscall.SIGINT).Funcs(), atreyu.Rider(syscall.SIGINT).Sequence())
+
+	// try another ctl-x here?
 
 	select {}
 }
